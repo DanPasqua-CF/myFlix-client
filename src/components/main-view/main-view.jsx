fc33d715cac0a8da58200a9ff5a228be12628453
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-05-29 16:24:14 -0400

    Update main-view with Row

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import MovieCard from "../movie-card/movie-card"
 import MovieView from "../movie-view/movie-view";
 import LoginView from "../login-view/login-view";
 import SignupView from "../signup-view/signup-view";
+import Row from "react-bootstrap/Row";
 
 const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
@@ -33,6 +34,42 @@ const MainView = () => {
       setMovies(moviesFromApi);
     });
   }, [token]);
+
+
+
+  return (
+    <Row>
+      {!user ? (
+        <>
+          <LoginView 
+          onLoggedIn={(user) => { 
+            setUser(user); 
+            setToken(token); 
+            localStorage.clear(); 
+          }} 
+          />
+          or
+          <SignupView />
+        </>
+      ) : selectedMovie ? (
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      ): movies.length === 0 ? (
+        <div>The list is empty</div>
+      ) : (
+        <>
+          {movies.map((movie) => {
+            <MovieCard key={movie.id} movie={movie} onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }} 
+            />
+          })}
+        </>
+      )}
+    </Row>
+  )
+
+
+
 
   if (!user) {
     return (
